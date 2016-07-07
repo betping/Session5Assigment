50f386f1a9932870181c597434e0d96aa726d2b0
betping <betping1993@gmail.com>
2016-07-08 04:30:40 +0530

    Add files via upload

---
+++ OverloadDemo.java
@@ -0,0 +1,17 @@
+
+public class OverloadDemo {
+	
+	    void area(double d)
+	    {
+	        System.out.println("the area of the square is "+Math.pow(d, 2)+" sq units");
+	    }
+	    void area(float x, float y)
+	    {
+	        System.out.println("the area of the rectangle is "+x*y+" sq units");
+	    }
+	    
+	    {
+	        
+	        
+	    }
+}
---
+++ overload.java
@@ -0,0 +1,13 @@
+
+public class overload {
+	class Overload 
+	{
+	     public void main(String args[]) 
+		{
+		   OverloadDemo ob = new OverloadDemo();
+		   ob.area(5);
+		   ob.area(11,12);
+		   ob.area(2.5);
+	        }
+	}
+}
